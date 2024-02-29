aba8175dbdb100da517004d07237fecd950e52b5
KDNguy <150972293+KDNguy@users.noreply.github.com>
2024-02-29 14:56:47 -0800

    Update 74HC165N_Top_Board.ino

--- Arduino/74HC165N_Top_Board/74HC165N_Top_Board.ino
+++ Arduino/74HC165N_Top_Board/74HC165N_Top_Board.ino
@@ -1,5 +1,5 @@
 /*
-Project: 74HC1656N Shift Register Top Board
+Project: 74HC165N Shift Register Top Board
 Programmer: Kevin Nguyen
 Datasheet: https://www.ti.com/lit/ds/symlink/sn74hc165.pdf
 Description:
@@ -7,13 +7,18 @@ Allows data from top board to be sent to main board using four connections inste
 The data sent will be the states of M1, M2, M3, A, B, DPI.
 */
 
+// Define globals (change as necessary)
+byte lastData = 0;
+byte data = 0;
+const int mouseDelay = 5;
+
 // Define pin connections (change as necessary)
 const int load = 8; // PL pin 1: When low, the parallel inputs are loaded. The clock is also ignored at this time. When high, the clock is able to be read.
 const int clock = 5;  // CP pin 2: When pulsed, the parallel inputs are push to the shift registers.
 const int clockEnable = 6;  // CE pin 15: When low, the clock is able to be pulsed. When high, the clock cannot pulse.
 const int dataIn = 7;  // Q7 pin 9: Serial output of shift register.
 
-void setup
+void setup()
 {
   // serial monitor
   Serial.begin(9600);
@@ -25,7 +30,7 @@ void setup
   pinMode(dataIn, INPUT);
 }
 
-void loop
+void loop()
 {
   // PL pulse
   digitalWrite(load, LOW);  // turn PL low to load inputs
@@ -36,18 +41,23 @@ void loop
   // push data to shift registers
   digitalWrite(clock, HIGH);  // pulse CP with CE low to push inputs
   digitalWrite(clockEnable, LOW);
-  byte data = shiftIn(dataIn, clock, LSBFIRST); // shift in data
-  digitalWrite(clock,, LOW);  // end of CP pulse
+  lastData = data;
+  data = shiftIn(dataIn, clock, LSBFIRST); // shift in data
+  digitalWrite(clockEnable, HIGH);  // end of CP pulse
 
   // decode byte data
-  Serial.print("States: ");
-
-  for (int i = 0; i < 8; i+)  // note: bits 7 and 8 should be zero because only 6 bits are written to
+  if (lastData != data)
   {
-    // output to serial monitor (or uC)
-    Serial.print(bitRead(incoming, i)); // shift register inverts bits
+    Serial.print("States: ");
+
+    for (int i = 0; i < 8; i++) // expected output: HGFEDCBA because LSB first
+    {
+      // output to serial monitor (or uC)
+      Serial.print(bitRead(data, i)); // shift register inverts bits
+    }
+
+    Serial.print("\n");
   }
 
-  Serial.print("\n");
-  delayMicroseconds(5);
+  delayMicroseconds(mouseDelay);
 }
